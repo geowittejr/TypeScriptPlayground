a71cae208932f672cc0edb4fec169a6bb87f5dd3
George Witte <geowittejr@gmail.com>
2016-09-19 08:09:45 -0400

    Added tsconfig.json file

--- js/Person.ts
+++ js/Person.ts
@@ -1,3 +1,22 @@
 /**
  * Created by George on 9/18/2016.
  */
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
