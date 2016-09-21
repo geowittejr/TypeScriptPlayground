37b39415d8bb681162058c32efdf629be80fd53c
George Witte <geowittejr@gmail.com>
2016-09-21 09:25:34 -0400

    Added link

--- js/Person.js
+++ js/Person.js
@@ -1,8 +1,5 @@
 /**
  * Created by George on 9/18/2016.
- *
- * https://www.typescriptlang.org/docs/tutorial.html
- *
  */
 var Student = (function () {
     function Student(firstName, middleInitial, lastName) {
