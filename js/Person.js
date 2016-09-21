5670e2b19ecf5aa898492bc7ddaf17c8dbec1aee
George Witte <geowittejr@gmail.com>
2016-09-21 09:44:24 -0400

    Added link to tutorial and also added middleInitial

--- js/Person.js
+++ js/Person.js
@@ -1,5 +1,7 @@
 /**
  * Created by George on 9/18/2016.
+ *
+ * https://www.typescriptlang.org/docs/tutorial.html
  */
 var Student = (function () {
     function Student(firstName, middleInitial, lastName) {
@@ -11,7 +13,7 @@ var Student = (function () {
     return Student;
 }());
 function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.firstName + " " + person.middleInitial + " " + person.lastName;
 }
 var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
